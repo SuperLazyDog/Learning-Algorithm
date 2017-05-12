f70d195e44caac58e44d51ab5e05f0700858125f
SuperLazyDog <jyoitatsu@foxmail.com>
2017-05-12 23:31:46 +0900

    Reverse Array

--- Basic/第二章/Array/Array/main.c
+++ Basic/第二章/Array/Array/main.c
@@ -10,11 +10,21 @@
 #include <stdlib.h>
 #include <time.h>
 #define N 5
+#define ARRAY_SIZE 15
+enum whether {
+    true = 1,
+    false = 0
+};
+typedef enum whether bool;
 // 数组的使用
 void useArray();
 int testFunc(int a);
 // 随机数的生成
 void createRandomTest();
+//逆序数组
+bool getRandomArray(int howMany, int *arrayTemp);
+bool reverseArray(int *array);
+bool showArray(int *array);
 
 int main(int argc, const char * argv[]) {
     //--------------------------------------------------------------------------
@@ -24,7 +34,20 @@ int main(int argc, const char * argv[]) {
     //--------------------------------------------------------------------------
     //                              随机数的生成
     //--------------------------------------------------------------------------
-    createRandomTest();
+    //createRandomTest();
+    //--------------------------------------------------------------------------
+    //                               逆序数组
+    //--------------------------------------------------------------------------
+    //getRandomArray(14);
+    //int temp[128] = {1,2,3,4,5};
+    //int *array = temp;//getRandomArray(15);
+    int *array = (int *)malloc(ARRAY_SIZE*sizeof(int));
+    getRandomArray(ARRAY_SIZE, array);
+    showArray(array);
+    reverseArray(array);
+    showArray(array);
+    printf("%d %d %d\n", *(array+3), *array, *(array+1));
+    free(array);
     return 0;
 }
 //-------------------------------------------------------------------------------------------
@@ -72,4 +95,39 @@ void createRandomTest() {
     for(i = 0; i < 30; i++){
         printf("%2d回: %d\n", i+1, rand()%10);
     }
+}
+//--------------------------------------------------------------------------
+//                               逆序数组
+//--------------------------------------------------------------------------
+//获取随机的整数数组
+bool getRandomArray(const int howMany, int *arrayTemp) {
+    int j = 0;
+    const unsigned seed = (unsigned int)time(NULL);
+    srand(seed);
+    for(j=0; j<howMany; j++) {
+        *arrayTemp = j;//rand();
+        arrayTemp++;
+    }
+    arrayTemp -= ARRAY_SIZE;
+    return true;
+}
+//把数组倒序
+bool reverseArray(int *array) {
+    int i;
+    for(i=0; i<ARRAY_SIZE/2; i++) {
+        int temp = array[i];
+        array[i] = array[ARRAY_SIZE-1-i];
+        array[ARRAY_SIZE-1-i] = temp;
+    }
+    return true;
+}
+//输出数组
+bool showArray(int *array) {
+    int i = 0;
+    for(i = 0; i < ARRAY_SIZE; i++) {
+        printf("第%2d个: %d\n", i, *array);
+        array++;
+    }
+    array -= ARRAY_SIZE;
+    return true;
 }
